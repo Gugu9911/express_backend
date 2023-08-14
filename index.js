9acd97d5af50226396c8e40685c7cb45bed67553
Gugu9911 <93252336+Gugu9911@users.noreply.github.com>
2023-08-14 16:37:38 +0300

    production_v3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./app')
+const app = require('./app')
 const http = require('http')
 const server = http.createServer(app)
+
 const config = require('./utils/config')
 server.listen(config.PORT, () => {
     logger.info(`Server is running on port ${config.PORT}`)
